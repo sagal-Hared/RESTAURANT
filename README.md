f645ead9ac82a3ae27527c25432fa255fa3816bd
Sagal-Hared <sagalhared2@gmail.com>
2024-06-06 15:47:15 +0300

    added script.js, made resposive design

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 
-ChatGPT
 A restaurant is a business establishment that prepares and serves food and drinks to customers. Restaurants vary widely in appearance and offerings, ranging from simple fast food eateries to high-end fine dining establishments. They can specialize in various cuisines, such as Italian, Chinese, or Mexican, and often provide a menu of dishes for customers to choose from.
--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const nav = document getelement
+let bars = document.getElementById("bars");
+let navRight = document.getElementById("nav-right");
+// let welcome = document.getElementById("welcome");
+// let ourCustomer=document.getElementById("our-customer")
+bars.addEventListener('click', () => {
+    navRight.classList.toggle('togglenav');    
+
+});
+
+// welcome.addEventListener('click', ()=>{
+// welcome.style.background="black"
+// })
+
+// ourCustomer.addEventListener("click", () =>{
+//     ourCustomer.style.background='red'
+// } )
